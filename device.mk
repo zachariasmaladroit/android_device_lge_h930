0119f76516769f95ba4f0c720b9d16db190ce76f
SGCMarkus <markusornik@gmail.com>
2018-05-22 15:57:24 +0200

    h930: made sensor configs common
    Change-Id: Ic5913d7aa58b9b2d6a4b1e0c2e2cc6669245d065

--- device.mk
+++ device.mk
@@ -21,7 +21,3 @@ $(call inherit-product-if-exists, vendor/lge/h930/h930-vendor.mk)
 
 # common v30
 $(call inherit-product, device/lge/joan-common/joan.mk)
-
-# Sensors
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_joan_global_com.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_variable.conf
